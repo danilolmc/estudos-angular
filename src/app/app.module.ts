9bae3ff6795592a642dc06510e291a94c985421b
DaniloLima122 <limadanilo06@gmail.com>
2021-05-04 19:04:20 -0300

    feat(tab): Added tab component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonModule } from './components/button/button.module';
 import { RippeEffectDirective } from './directives/ripple-effect/rippe-effect.directive';
+import { TabComponent } from './components/tab/tab.component';
+import { TabItemComponent } from './components/tab/tab-item/tab-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RippeEffectDirective,
+    TabComponent,
+    TabItemComponent,
   ],
   imports: [
     BrowserModule,
